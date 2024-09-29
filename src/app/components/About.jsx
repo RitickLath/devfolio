21f77fe2c580118dd24e51ae5c623c8b8447f9f1
RitickLath <ritick943lath@gmail.com>
2024-09-30 01:44:21 +0530

    Button

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,46 +1,49 @@
 import React from "react";
+import { Button } from "./ui/moving-border";
 
 const About = () => {
   return (
-    <div className="mt-20">
+    <div className="my-10 lg:mt-20">
       <h1 className="text-xl sm:text-2xl md:text-3xl text-[#22C55E] font-semibold mb-2 lg:mb-4">
         Who am I?
       </h1>
       <div className="md:flex justify-between">
-        <div className="md:w-[300px] lg:w-[500px]">
-          <p>
+        <div className="max-w-[500px] ">
+          <p className="mb-4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est
             assumenda ipsa illum nostrum dignissimos repudiandae esse eveniet
             iste molestiae quis similique accusamus dolorem cupiditate, nobis
             sit? Doloribus inventore id eveniet?
           </p>
-          <button className="bg-[#22C55E] mt-4 text-white py-1 px-4 rounded-md">
-            Download CV
-          </button>
+
+          <Button className="font-medium">Download CV</Button>
         </div>
 
-        <div className="grid sm:grid-cols-2 mt-6 md:mt-0">
+        <div className="hidden lg:flex mt-6 md:mt-0">
           <div>
-            <h1 className="text-white">
-              Name: <span className="text-[#7A7A7A]">Ritick Lath</span>
-            </h1>
-          </div>
-          <div>
-            <h1 className="text-white">
-              Education:{" "}
-              <span className="text-[#7A7A7A]">Bachelors of Technology</span>
-            </h1>
-          </div>
-          <div>
-            <h1 className="text-white">
-              Profession:{" "}
-              <span className="text-[#7A7A7A]">Software Developer</span>
-            </h1>
-          </div>
-          <div>
-            <h1 className="text-white">
-              Location: <span className="text-[#7A7A7A]">India</span>
-            </h1>
+            <div>
+              <h1 className="text-white">
+                Name: <span className="text-[#7A7A7A]">Ritick Lath</span>
+              </h1>
+            </div>
+
+            <div>
+              <h1 className="text-white">
+                Education:{" "}
+                <span className="text-[#7A7A7A]">Bachelors of Technology</span>
+              </h1>
+            </div>
+            <div>
+              <h1 className="text-white">
+                Profession:{" "}
+                <span className="text-[#7A7A7A]">Software Developer</span>
+              </h1>
+            </div>
+            <div>
+              <h1 className="text-white">
+                Location: <span className="text-[#7A7A7A]">India</span>
+              </h1>
+            </div>
           </div>
         </div>
       </div>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import profilePic from "@/app/components/profile-pic.webp";
+import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   const el = useRef(null);
@@ -26,7 +27,7 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <div className="md:flex md:justify-between">
+    <div className="lg:flex lg:justify-between">
       {/* Text Section */}
       <div>
         <div className="mt-[100px]">
@@ -39,14 +40,12 @@ const HeroSection = () => {
             I make the complex Simple.
           </h1>
           <div className="mt-6 lg:mt-12">
-            <button className="bg-[#22C55E] text-white py-2 px-4 rounded-md">
-              Contact me
-            </button>
+            <Button className="font-medium">Contact Me</Button>
 
             {/* Contact Info Section */}
             <div className="mt-12 hidden sm:text-sm sm:flex sm:flex-row sm:space-x-6 sm:space-y-0 space-y-6">
               {/* Email Section */}
-              <div className="text-center sm:text-left">
+              <div className="text-center sm:text-left text-sm">
                 <h1 className="text-white font-semibold">Email</h1>
                 <h1 className="text-white">ritick943lath@gmail.com</h1>
               </div>
@@ -74,8 +73,20 @@ const HeroSection = () => {
       </div>
 
       {/* Image Section */}
-      <div className="hidden sm:flex md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] justify-center sm:justify-normal mt-[20px] lg:mt-[100px]">
-        <Image src={profilePic} alt="Profile Pic" height={400} width={400} />
+      <div className="flex space-x-4 sm:space-x-12 mt-[20px] lg:mt-[100px]">
+        <Image
+          className="w-[220px] h-[220px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] "
+          src={profilePic}
+          alt="Profile Pic"
+          height={400}
+          width={400}
+        />
+        <div className="flex text-white lg:hidden md:mt-[260px] mt-[167px]">
+          <div>
+            <h1>Name: Ritick Lath</h1>
+            <h1>Location: New Delhi</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
